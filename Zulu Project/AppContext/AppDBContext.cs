8fca59190f86a0bfe2b93f927d88e178c80a7be6
Mohammed Abbas <mohammed-abbas0@outlook.com>
2023-08-10 17:25:53 +0300

    Create Company Model.

---
+++ Zulu Project/AppContext/AppDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Zulu_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspCoreApI_Project.AppContext
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Company> Companies { get; set; }
+    }
+}
---
+++ Zulu Project/Models/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zulu_Project.Models
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public string CompanyCode { get; set; }
+        public int? Employees { get; set; }
+        [Display(Name = "Date of Establishment")]
+        public DateTime? Created { get; set; }
+    }
+
+}
