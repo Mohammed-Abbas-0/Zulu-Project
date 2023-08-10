d91c4d068b05251a8a74c66a6fe01e9fb1187b2d
Mohammed Abbas <mohammed-abbas0@outlook.com>
2023-08-10 16:42:46 +0300

    Revert "Revert "Create Company Model""
    This reverts commit c77ec14af3bf3a3ed34d8fe766517ad3ad0d2d4c.

---
+++ Zulu Project/Migrations/20230810132420_Create Company Table.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Zulu_Project.Migrations
+{
+    public partial class CreateCompanyTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Employees = table.Column<int>(type: "int", nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
